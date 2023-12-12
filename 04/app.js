5477392297e5ef1beb892ce804f8b255244a1343
Artur Matysiak <inz.matys@gmail.com>
2023-12-12 14:46:04 +0100

    task done, now

--- 04/app.js
+++ 04/app.js
@@ -1,11 +1,20 @@
 const divs = document.querySelectorAll("div");
 
 // add class 'clicked'
-divs.forEach((div) => div.addEventListener("click", (e) => e.target.classList.add("clicked")));
+addClass = (target) => target.classList.add("clicked");
+
+divs.forEach((div) =>
+  div.addEventListener("click", (e) => {
+    if (e.target.className == "child") {
+      setTimeout(() => addClass(e.target), e.target.dataset.time);
+      setTimeout(() => addClass(e.target.parentElement), e.target.parentElement.dataset.time);
+      setTimeout(() => addClass(e.target.parentElement.parentElement), e.target.parentElement.parentElement.dataset.time);
+    } else setTimeout(() => addClass(e.target), e.target.dataset.time);
+  })
+);
 
 // remove class 'clicked' after click to <body>
+removeClass = (target) => target.classList.remove("clicked");
+
 const myBody = document.querySelector("body");
-myBody.addEventListener("click", (e) => {
-  const myClick = e.target;
-  myClick == myBody ? divs.forEach((div) => div.classList.remove("clicked")) : null;
-});
+myBody.addEventListener("click", (e) => (e.target == myBody ? divs.forEach((div) => removeClass(div)) : null));
